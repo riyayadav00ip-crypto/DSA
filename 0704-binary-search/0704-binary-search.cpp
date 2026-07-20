a609775b91aadea75c38c553e2203e0d87636182
Riya yadav <riyayadav00ip@gmail.com>
2026-07-21 01:43:17 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.82%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -1,16 +1,13 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int n = nums.size();
         int start = 0;
-        int end = n - 1;
+        int end = nums.size() - 1;
        
-        
-        
         while(start <= end) {
 
             int mid = (start + end) / 2;
-            
+
             if(nums[mid] == target) {
                 return mid;     
             }
